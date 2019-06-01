bc6c80743fff2e706e985252a49eaf134d5e32d4
Bahati <gur79tub@gmail.com>
2019-06-01 15:23:44 +0200

    NotFound page

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => (
+  <section id="not-found">
+    <div className="hero is-fullheight is-primary">
+      <div className="hero-body">
+        <div className="container has-text-center">
+          <h1 className="title is-1">
+            4 <i className="fa fa-ban" /> 4
+          </h1>
+          <h1 className="title is-4">
+            The Page you are looking for is not exists
+          </h1>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default NotFound;
