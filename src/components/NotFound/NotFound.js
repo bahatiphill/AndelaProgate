a2bfcfb481e1dcb61854964100d880117ea47c54
Bahati <gur79tub@gmail.com>
2019-06-01 17:36:26 +0200

    alpha version

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,15 +3,16 @@ import "./NotFound.css";
 
 const NotFound = () => (
   <section id="not-found">
-    <div className="hero is-fullheight is-primary">
+    <div className="hero is-fullheight is-info">
       <div className="hero-body">
-        <div className="container has-text-center">
+        <div className="container has-text-centered">
           <h1 className="title is-1">
             4 <i className="fa fa-ban" /> 4
           </h1>
-          <h1 className="title is-4">
-            The Page you are looking for is not exists
-          </h1>
+
+          <h4 className="title is-4">
+            The page you are looking for was not found
+          </h4>
         </div>
       </div>
     </div>
